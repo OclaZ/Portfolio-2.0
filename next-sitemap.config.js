0a5f753758a9572bb1d7529dc94f8b2ec391b0e0
OclaZ <aslikhhamza2001@gmail.com>
2024-12-15 12:02:29 +0100

    ss

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://hamzaaslikh.com", // Replace with your domain
-  generateRobotsTxt: true, // Generates robots.txt
-  sitemapSize: 5000, // Limit the number of entries per sitemap file
-  generateIndexSitemap: true, // Ensures a sitemap index is created
+  siteUrl: "https://yourdomain.com", // Replace with your actual domain
+  generateRobotsTxt: true, // Optionally generates robots.txt
+  sitemapSize: 5000, // Max number of URLs per sitemap file
+  generateIndexSitemap: true, // Ensures the sitemap index is created
 };
