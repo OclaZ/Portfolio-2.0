a74098dd3c6cfba781815c2eda089ab9410849f9
OclaZ <aslikhhamza2001@gmail.com>
2024-12-15 11:44:18 +0100

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://hamzaaslikh.com", // Replace with your actual domain
+  generateRobotsTxt: true, // Generate robots.txt file
+  exclude: [], // Add any paths you want to exclude
+};
